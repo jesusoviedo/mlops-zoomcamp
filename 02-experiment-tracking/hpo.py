6a20016c7938a88455fbca785825f5fb7c077960
Jesús Oviedo Riquelme <j92riquelme@gmail.com>
2024-05-27 18:44:40 +0000

    update func root_mean_squared_error

--- 02-experiment-tracking/hpo.py
+++ 02-experiment-tracking/hpo.py
@@ -6,7 +6,7 @@ import numpy as np
 from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
 from hyperopt.pyll import scope
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 mlflow.set_tracking_uri("sqlite:///mlflow.db")
 mlflow.set_experiment("random-forest-hyperopt")
@@ -40,7 +40,7 @@ def run_optimization(data_path: str, num_trials: int):
             rf = RandomForestRegressor(**params)
             rf.fit(X_train, y_train)
             y_pred = rf.predict(X_val)
-            rmse = mean_squared_error(y_val, y_pred, squared=False)
+            rmse = root_mean_squared_error(y_val, y_pred)
 
             mlflow.log_metric("rmse", rmse)
             
