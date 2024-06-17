00efa0ea00368ccf898586472e15aea557039943
Jesús Oviedo Riquelme <j92riquelme@gmail.com>
2024-06-17 09:22:32 -0400

    practica calculo online por ws

---
+++ 04-deployment/webservice/predict.py
@@ -0,0 +1,44 @@
+import pickle
+
+from flask import Flask, request, jsonify
+
+with open('lin_reg.bin', 'rb') as f_in:
+    (dv, model) = pickle.load(f_in)
+
+
+def prepare_features(ride):
+    features = {}
+    #features['PU_DO'] = '%s_%s' % (ride['PULocationID'], ride['DOLocationID'])
+    #features['trip_distance'] = ride['trip_distance']
+    features["PULocationID"] = ride['PULocationID']
+    features["DOLocationID"] = ride['DOLocationID']
+    return features
+
+
+def predict(features):
+    X = dv.transform(features)
+    preds = model.predict(X)
+    return float(preds[0])
+
+
+app = Flask('duration-prediction')
+
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    ride = request.get_json()
+
+    features = prepare_features(ride)
+    pred = predict(features)
+
+    result = {
+        'duration': pred
+    }
+
+    return jsonify(result)
+
+#para prod se recomienda usar un servidor como:
+# gunicorn -> https://gunicorn.org/
+# waitress -> https://flask.palletsprojects.com/en/3.0.x/deploying/waitress/
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
---
+++ 04-deployment/webservice/test.py
@@ -0,0 +1,12 @@
+#usamos pipenv para la gestion de paquetes en lugar de conda o envpip -> https://pipenv.pypa.io/en/latest/installation.html
+
+import requests
+
+ride = {
+    "PULocationID": 10,
+    "DOLocationID": 50,
+}
+
+url = 'http://localhost:9696/predict'
+response = requests.post(url, json=ride)
+print(response.json())
